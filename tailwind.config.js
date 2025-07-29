d1d8bfb6a7e52084f0665b2374400d064134667f
mosinf <mosinf@scribd.com>
2025-07-29 12:33:32 +0530

    added redux toolkit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // for Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // include all source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html", // for Vite
+//     "./src/**/*.{js,ts,jsx,tsx}", // include all source files
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
